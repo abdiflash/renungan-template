828af5f5319853db6812d1caa69b859fbafaf0c7
abdiflash <abdiflash@gmail.com>
2026-02-20 22:39:22 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    SCHOOL_NAME: "Nama Sekolah",
+    SCHOOL_SITE: "sekolah.sch.id",
+    SCHOOL_URL: "https://sekolah.sch.id",
+
+    SHEET_ID: "",
+    SHEET_NAME: "renungan",
+    SHEET_GID: "0",
+
+    COUNTER_NAMESPACE: "template-renungan",
+    COUNTER_KEY: "visitor_count"
+};
