a5ec35ffc270ea5215a646014e0e5f1ce4d5fedc
abdiflash <abdiflash@gmail.com>
2026-09-04 04:53:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 const CONFIG = {
+    // URL Web App dari Google Apps Script
+    API_URL: "https://script.google.com/macros/s/AKfycbwvbxLRKj2Mrd8c4sAgHKW14qeDht7t4jDYno2j-Apm0lULb3iLCPGwVSwTxVGLRGcX/exec",
+
     SCHOOL_NAME: "Template Renungan Harian",
     SCHOOL_SITE: "edueasy.id",
     SCHOOL_URL: "https://edueasy.id",
